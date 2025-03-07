5d3cdae768e0701f60409bf4b4c3f3dc9dd82e27
Sujetha Pai Krishna Pai <122850668+suthapai@users.noreply.github.com>
2025-03-07 12:29:47 -0500

    Add files via upload

---
+++ private-acr/provider.tf
@@ -0,0 +1,10 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    
+  }
+}
